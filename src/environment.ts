1c9df339512794faa46de6f823de8825a3dea879
Bernardo Garza <bgarza@bjntech.com>
2020-06-12 20:28:06 -0500

    Added a production variable to the environment folder

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,7 @@
-import { dirname } from "path";
+import { dirname, extname as fileExtension } from "path";
 import { Platforms } from "./cat-com-structs";
 
 export const currentPlatform: Platforms = <Platforms>process.platform;
 export const currentPath: string = dirname(process.execPath);
+
+export const production: boolean = fileExtension(process.argv[1]) === ".js";
